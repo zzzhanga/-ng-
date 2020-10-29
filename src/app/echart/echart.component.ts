3e2045ce4a37fdb34c39bd743788440b7166ad37
zhanghang <palmer_zhang@enable-ets.com>
2020-10-29 15:00:58 +0800

    引入echarts依赖成功

---
+++ src/app/echart/echart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-echart',
+  templateUrl: './echart.component.html',
+  styleUrls: ['./echart.component.css']
+})
+export class EchartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
